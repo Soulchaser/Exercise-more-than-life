b847e8e4f553e4fc270ca8faf4c73dab7dba9977
949961535 <949961535@qq.com>
2016-01-16 21:16:29 +0800

    YY LeancoudTools类基本完成

--- teamWork/Source/BaseClass/LeancoudTools.m
+++ teamWork/Source/BaseClass/LeancoudTools.m
@@ -24,6 +24,7 @@
         if (user != nil) {
             result(YES,nil);
         }else{
+            //登陆失败 根据error.code 查找失败原因
             result(NO,error);
         }
     }];
@@ -40,30 +41,59 @@
             //注册成功
             result(YES,nil);
         }else {
+            //注册失败
             result(NO,error);
         }
     }];
 }
 //修改密码
-//填入邮箱, 邮箱验证正确后会向邮箱发送修改密码的邮件
+//旧密码替代新密码
 -(void)userChangePasswordWithNewPassword:(NSString *)newPassword resultBlock:(yyResultPassBlock)result{
-    //先进行登陆
+    //先进行登陆 修改当前用户的密码
     [AVUser logInWithUsername:self.model.userName password:self.model.password error:nil];
     //修改当前用户的密码
     [[AVUser currentUser] updatePassword:self.model.password newPassword:newPassword block:^(id object, NSError *error) {
-        if (object) {
-            
+        if (error == nil) {
+            //修改成功
+            result(YES,nil);
+            //修改成功 退出当前用户
+            [AVUser logOut];
+        }else {
+            //修改失败
+            result(NO,error);
         }
     }];
-    
-    
 }
-//忘记密码 进行重置
-
--(void)userResetPassword:(yyResultPassBlock)result{
-    
+//找回密码
+//填入邮箱, 邮箱验证正确后会向邮箱发送重置密码的邮件
+-(void)userResetPasswordWithEmail:(NSString *)email resultBlock:(yyResultPassBlock)result{
+    [AVUser requestPasswordResetForEmailInBackground:email block:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            //邮件发送成功
+            result(YES,nil);
+        }else {
+            //找回失败(邮件发送失败)
+            result(NO,error);
+        }
+    }];
+}
+//根据用户名验证用户是否已注册过
+-(void)userDidBecomeRegisterWithUserName:(NSString *)userName resultBlock:(yyResultPassBlock)result{
+    AVQuery *query = [AVQuery queryWithClassName:@"_User"];
+    [query whereKey:@"username" equalTo:userName];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (objects.count > 0) {
+            //用户存在
+            result(YES,nil);
+        }else {
+            //用户不存在
+            result(NO,nil);
+        }
+    }];
 }
 
 
 
 @end
+
+
