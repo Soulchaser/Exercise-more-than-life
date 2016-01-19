85639f414a3d597a8faecb9a0b94b9d82c2bd394
hanxiaolong <hanxiaolong1116@163.com>
2016-01-19 20:58:56 +0800

    完成了地图界面全部的控件布局，优化以控件的图片

---
+++ teamWork/Source/CodeClass/View/map/MapMoveInfoView.h
@@ -0,0 +1,22 @@
+//
+//  MapMoveInfoView.h
+//  teamWork
+//
+//  Created by hanxiaolong on 16/1/19.
+//  Copyright © 2016年 hanxiaolong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapMoveInfoView : UIView
+
+
+@property(strong,nonatomic)UILabel *distanceTilelLabel;
+@property(strong,nonatomic)UILabel *speedTilelLabel;
+@property(strong,nonatomic)UILabel *timeTilelLabel;
+@property(strong,nonatomic)UILabel *distanceLabel;
+@property(strong,nonatomic)UILabel *speedLabel;
+@property(strong,nonatomic)UILabel *timeLabel;
+
+
+@end
