09cd47936ad58c2713f38a80d288694f93c7dc10
949961535 <949961535@qq.com>
2016-01-20 08:59:02 +0800

    完成手机注册Leancoud类

--- teamWork/Source/CodeClass/Model/User/YYUserModel.h
+++ teamWork/Source/CodeClass/Model/User/YYUserModel.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#pragma mark --- 邮箱--------------------
 //用户信息model(存储在leancoud上)  用户登陆时, 用邮箱+密码登陆  昵称用来展示
 @interface YYUserModel : NSObject
 @property(strong,nonatomic)NSString *userName;//昵称
@@ -26,12 +27,30 @@
 @property(strong,nonatomic)NSString *shareTime;//分享时间
 @property(strong,nonatomic)NSData *shareDetail;//分享内容
 
-
 @end
 
-//用户评论model(评论时需要的信息)
+//用户评论model(评论时需要展示的信息)
 @interface YYUserComment : NSObject
 @property(strong,nonatomic)NSString *userName;//用户名
 @property(strong,nonatomic)NSString *commentDetail;//评论内容
 
 @end
+#pragma mark ------ 手机号------------------------
+//用户信息model(存储在leancoud上)  用户登陆时, 用手机号+密码登陆  昵称用来展示
+@interface YYUserModel_Phone : NSObject
+@property(strong,nonatomic)NSString *userName;//昵称
+@property(strong,nonatomic)NSString *password;//密码
+@property(strong,nonatomic)NSString *phone;//手机号
+@property(strong,nonatomic)NSString *gender;//性别
+@property(strong,nonatomic)NSString *age;//年龄
+@property(strong,nonatomic)UIImage *headerImage;//头像
+@property(strong,nonatomic)NSMutableArray *joinActivity;//参加的活动
+@end
+
+
+
+
+
+
+
+
